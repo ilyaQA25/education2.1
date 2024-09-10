15585b666677197f49067227e5f62aa6f94cb1b5
ilya2.0 <iprez4031@gmail.com>
2024-09-10 23:18:42 +0300

    наследование, дз по фигурам абстрактные классы и интерфейсы gitignore->.idea

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Rectangle rectangle2 = new Rectangle(2,3);
         Rectangle rectangle3 = new Rectangle(3,4);
         Rectangle rectangle4 = new Rectangle(5,6);
+        Rectangle rectangle5 = new Rectangle(5,6);
         Rectangle[] arrRect = new Rectangle[]{rectangle1,rectangle2,rectangle3,rectangle4};
         for (int i = 0; i < arrRect.length; i++) {
             System.out.println(arrRect[i].getPerim());
