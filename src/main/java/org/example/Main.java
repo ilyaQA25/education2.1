e2577b070e1653aca596578edafa4a4310ef4227
ilya2.0 <iprez4031@gmail.com>
2024-09-10 23:36:48 +0300

    gitignore->.idea clear master 2.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,10 @@
 package org.example;
 
-import org.example.HWFigures.Rectangle;
+import org.example.Main;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle1 = new Rectangle(1,2);
-        Rectangle rectangle2 = new Rectangle(2,3);
-        Rectangle rectangle3 = new Rectangle(3,4);
-        Rectangle rectangle4 = new Rectangle(5,6);
-        Rectangle[] arrRect = new Rectangle[]{rectangle1,rectangle2,rectangle3,rectangle4};
-        for (int i = 0; i < arrRect.length; i++) {
-            System.out.println(arrRect[i].getPerim());
+
 
         }
     }
-}
