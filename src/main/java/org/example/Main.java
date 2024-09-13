fd3b084d1d49e9d0d130cef26846037bcd2ce2a7
ilya2.0 <iprez4031@gmail.com>
2024-09-13 22:36:40 +0300

    Lesson8 - enum seasons вычисление пары года по номеру месяца

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,24 @@
 package org.example;
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        TestRecord testRecord = new TestRecord(50,50);
+        int month = 5;
 
-        System.out.println(testRecord.OverallIncome());
+
+        Seasons seasons = Seasons.fromMonth(month);
+
+        if(seasons == Seasons.WINTER){
+            System.out.println("Winter");
+        } else if (seasons == Seasons.FALL) {
+            System.out.println("Fall");
+        } else if (seasons ==Seasons.SPRING) {
+            System.out.println("spring");
+        } else if (seasons ==Seasons.SUMMER) {
+            System.out.println("summer");
         }
+    }
     }
