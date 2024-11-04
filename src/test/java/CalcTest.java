5a7a6566f13d40b8f340851fb30de0925faf34fc
ilya2.0 <iprez4031@gmail.com>
2024-11-04 17:07:48 +0300

    qa auto allure-res ignore2

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -14,7 +14,7 @@ public class CalcTest extends BaseTest{
         System.out.println("calcTest started");
     }
 
-    @Test
+    @Test(testName = "fdgdf")
     public void testSum(){
         Assert.assertEquals(calc.sum(2,3), 5, "wrong sum");
     }
