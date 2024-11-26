7b4840d5f4f422e64cdbd161b90a9be0c09be777
ilya2.0 <iprez4031@gmail.com>
2024-11-26 20:32:06 +0300

    Selenium_web_driver_first-browser-setup

--- src/main/java/Calc.java
+++
@@ -1,9 +0,0 @@
-public class Calc {
-    public int sum(int a, int b){
-        return a+b;
-    }
-
-    public double division(double a, double b) throws ArithmeticException{
-        return a/b;
-    }
-}
--- src/main/java/services/SimpleDriver.java
+++ src/main/java/services/SimpleDriver.java
@@ -1,4 +1,4 @@
-package core;
+package services;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
