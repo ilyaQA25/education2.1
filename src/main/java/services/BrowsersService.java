0a042260963d5338eedd862c3dc62ee65b695ded
ilya2.0 <iprez4031@gmail.com>
2024-12-09 20:04:51 +0300

    laminat test done

--- src/main/java/services/BrowsersService.java
+++ src/main/java/services/BrowsersService.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
+
 public class BrowsersService {
     private WebDriver driver = null;
 
@@ -43,6 +45,7 @@ public class BrowsersService {
     public WebDriver getDriver() {
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
 
 
         return driver;
